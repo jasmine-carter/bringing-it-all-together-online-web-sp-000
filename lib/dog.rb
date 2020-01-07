a45c211f4f946c00efbb23a744d76213d2b17494
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-07 02:35:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,7 +77,8 @@ class Dog
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
-    DB[:conn].execute(sql, name)
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(result[0], result[1], result[2])
   end
 
 end
