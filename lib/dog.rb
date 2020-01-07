9d50322a69bd84c2efb9a867c6673f20b43aad23
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-07 02:26:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,8 +68,6 @@ class Dog
     found = DB[:conn].execute(sql, name, breed).flatten
     if found == nil
       Dog.new(id: id, name: name, breed: breed)
-    elsif self.id == nil
-      self.save
     else
       Dog.find_by_id(result[0])
     end
