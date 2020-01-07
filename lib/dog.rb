136310033176b4fffab23740c30cf8a922d9b550
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-07 01:40:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,11 @@ class Dog
     DB[:conn].execute(sql, name, breed, id)
   end
 
-  def self.new_from_db
-
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = [2]
+    Dog.new(id,name,breed)
   end
 
 end
